904fcfe7206392cad24c814c7d711d6b2b454e80
cieloazul310 <cieloazul310@gmail.com>
2023-11-01 14:35:50 +0900

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
+    "plugin:astro/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        "import/prefer-default-export": "off",
+        "import/no-extraneous-dependencies": "off"
+      },
+    },
+    {
+      files: ["astro.config.ts", "panda.config.ts"],
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+      },
+    },
+    {
+      files: ["src/content/config.ts"],
+      rules: {
+        "import/prefer-default-export": "off",
+      },
+    }
+  ],
+};
---
+++ astro.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+import pandacss from '@pandacss/astro';
+
+export default defineConfig({
+  integrations: [pandacss()],
+});
